c4b26564ef6efaa1f5c1ead6fa819e70fb2b60cd
James Chen-Smith <james@chen-smith.net>
2023-02-03 13:46:04 -0600

    Process Eleventy build output with Vite

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,22 @@
+const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
+
 module.exports = function (eleventyConfig) {
+    const output = 'public'
+
+    eleventyConfig.addPlugin(EleventyVitePlugin, {
+        viteOptions: {
+            // Must be different than Eleventy's output directory.
+            publicDir: `_${output}`
+        }
+    })
+
     eleventyConfig.addPassthroughCopy({ './site/static': '/' })
 
     return {
         dir: {
+            output,
             includes: '_includes',
             input: 'site',
-            output: 'public',
         },
     }
 }
